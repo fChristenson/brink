0f55f73dbd97eab50af6d824d01672756b9942bd
Fredrik <fredrik.christenson@gmail.com>
2019-10-19 20:09:02 +0200

    redux persist added

--- brink-editor/src/public/ts/main.tsx
+++ brink-editor/src/public/ts/main.tsx
@@ -1,22 +1,25 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
 import { FlowEditor } from "./components/FlowEditor";
 import "../libs/dragAndDrop";
 import { BrowserRouter, Route } from "react-router-dom";
 import { PageEditor } from "./components/PageEditor";
 import { Preview } from "./components/Preview";
 import { previewRoute, pageRoute, rootRoute } from "../libs/api/routes";
+import { PersistGate } from "redux-persist/integration/react";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Route exact path={rootRoute.template} component={FlowEditor} />
-        <Route exact path={pageRoute.template} component={PageEditor} />
-        <Route exact path={previewRoute.template} component={Preview} />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Route exact path={rootRoute.template} component={FlowEditor} />
+          <Route exact path={pageRoute.template} component={PageEditor} />
+          <Route exact path={previewRoute.template} component={Preview} />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 };
--- brink-editor/src/public/ts/store/store.ts
+++ brink-editor/src/public/ts/store/store.ts
@@ -1,8 +1,19 @@
 import { createStore } from "redux";
 import { reducers } from "./reducers";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = createStore(
-  reducers,
+  persistedReducer,
   // @ts-ignore
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+export const persistor = persistStore(store);
