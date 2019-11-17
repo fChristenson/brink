96bad63c89dc3f89a0433bbd14cdf1af1e36bfa9
Fredrik <fredrik.christenson@gmail.com>
2019-11-17 17:46:22 +0100

    fix flow export

--- brink-editor/src/libs/types/exportPage.ts
+++ brink-editor/src/libs/types/exportPage.ts
@@ -1,10 +1,13 @@
-import { IRootNode } from "../../public/ts/app/components/Canvas/components/RootNode/RootNode";
-
 export interface IExportPageRequest {
   name: string;
   xml: string;
 }
 
+export interface IFlowExportData {
+  title: string;
+  xmlCode: string;
+}
+
 export interface IExportFlowRequest {
-  rootNodes: IRootNode[];
+  rootNodes: IFlowExportData[];
 }
--- brink-editor/src/public/libs/export/api.ts
+++ brink-editor/src/public/libs/export/api.ts
@@ -3,12 +3,12 @@ import {
   IExportFlowRequest
 } from "../../../libs/types/exportPage";
 import { exportPage, exportFlow } from "../../../libs/routes";
-import { getRequestInit } from "../api/requestInit";
+import { Request } from "../api/requestInit";
 import { IRootNode } from "../../ts/app/components/Canvas/components/RootNode/RootNode";
 
 export const downloadTsxCode = async (name: string, xml: string) => {
   const body: IExportPageRequest = { name, xml };
-  const data = getRequestInit("POST", body);
+  const data = Request("POST", body);
   const res = await fetch(exportPage, data);
   const text = await res.text();
   const blob = new Blob([text]);
@@ -19,8 +19,10 @@ export const downloadTsxCode = async (name: string, xml: string) => {
 };
 
 export const downloadFlow = async (name: string, rootNodes: IRootNode[]) => {
-  const body: IExportFlowRequest = { rootNodes };
-  const data = getRequestInit("POST", body);
+  const body: IExportFlowRequest = {
+    rootNodes: rootNodes.map(n => ({ title: n.title, xmlCode: n.xmlCode }))
+  };
+  const data = Request("POST", body);
   const res = await fetch(exportFlow, data);
   const buf = await res.arrayBuffer();
   const link = document.createElement("a");
